0842e5b2f1286ec08e600840aaa37466561a865e
Williano <paawilly17@gmail.com>
2020-07-04 16:15:22 -0500

    Update

--- events/views/events_list_view.py
+++ events/views/events_list_view.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 
 def list_of_events(request):
-    pass
+    context_object = {}
+    template_name = "events/events_list.html"
+    
+    return render(request, template_name=template_name, context=context_object)
