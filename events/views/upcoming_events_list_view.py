883ce403f00477d843ffd4a4da23df72ee0373c4
Williano <paawilly17@gmail.com>
2020-07-04 20:57:42 -0500

    Update

--- events/views/upcoming_events_list_view.py
+++ events/views/upcoming_events_list_view.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 
 def list_of_upcoming_events(request):
-    pass
+    context_object = {}
+    template_name = "events/upcoming_events_list.html"
+
+    return render(request, template_name=template_name, context=context_object)
