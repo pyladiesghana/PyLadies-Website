cbf01d4358b6abd939eec70f300233d5c23fb4c0
Williano <paawilly17@gmail.com>
2020-07-04 21:56:58 -0500

    Add event model to admin

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,20 @@
+# Core Django imports.
 from django.contrib import admin
 
-# Register your models here.
+# Sponsor app imports.
+from events.models.event_model import Event
+
+
+class EventAdmin(admin.ModelAdmin):
+
+    list_display = ('event_title', 'event_image', 'event_description',
+                    'event_location', 'event_status',)
+    list_filter = ('event_title', 'event_status', 'event_location')
+    search_fields = ('event_title',)
+    date_hierarchy = 'created_on'
+    ordering = ['updated_on', ]
+    readonly_fields = ['created_on', 'updated_on']
+
+
+# Registers the partner model at the admin backend.
+admin.site.register(Event, EventAdmin)
