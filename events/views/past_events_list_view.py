fda5493c0a9910dbc6e0840ad0d4390caa5e7a27
Williano <paawilly17@gmail.com>
2020-07-04 20:57:22 -0500

    Update

--- events/views/past_events_list_view.py
+++ events/views/past_events_list_view.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 
 def list_of_past_events(request):
-    pass
+    context_object = {}
+    template_name = "events/past_events_list.html"
+
+    return render(request, template_name=template_name, context=context_object)
